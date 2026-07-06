e14fbbbed5d7142eadf4f1723e99a6dce75f9d17
heymans <heymanswang@outlook.com>
2026-07-06 16:29:15 +0800

    feat: 添加 ent ORM 和 User schema
    - 安装 entgo.io/ent 依赖
    - 创建 User schema（id, nickname, username, password, email）
    - 生成 ent client 代码

---
+++ backend/ent/schema/user.go
@@ -0,0 +1,47 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+func (User) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{
+			Table:   "users",
+			Options: "COMMENT='用户表'",
+		},
+		entsql.WithComments(true),
+	}
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int64("id").Unique().Positive().Immutable().Comment("用户ID"),
+		field.String("nickname").NotEmpty().Unique().Comment("昵称"),
+		field.String("username").NotEmpty().Unique().Comment("用户名"),
+		field.String("password").NotEmpty().Comment("密码"),
+		field.String("email").NotEmpty().Unique().Comment("邮箱"),
+	}
+}
+
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("username").Unique(),
+		index.Fields("email").Unique(),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
